046b2f9912eff388d59327033098c71c8c8dd1ce
Crom <navalcromwell@gmail.com>
2026-03-22 16:52:19 +0800

    changed Users endpoint to GetUsers

--- backend/first-spring/src/main/java/com/spring/Controller/UsersController.java
+++ backend/first-spring/src/main/java/com/spring/Controller/UsersController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Users")
+@RequestMapping("/GetUsers")
 public class UsersController {
     private final UserService userService;
 
