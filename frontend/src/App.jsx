7f16f52f2012bff9c0da635e807788d0ffba660b
eron381 <eronvidal07@gmail.com>
2026-04-11 21:30:51 +0800

    login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Navigate to="/admin/user-management" replace />} />
-        <Route path="/login" element={<Loginpage />} />
+          <Route path="/user-management" element={<Navigate to="/admin/user-management" replace />} />
+        <Route path="/" element={<Loginpage />} />
         <Route path="/admin/user-management" element={<UserManagement />} />
         <Route path="/frontdesk/schedules" element={<ScheduleManagement />} />
         <Route path="/frontdesk/rehab-dashboard" element={<RehabilitationDashboard />} />
