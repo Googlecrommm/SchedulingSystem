5537cecd10a2edde97d16b94ec9b456ca50203da
Crom <navalcromwell@gmail.com>
2026-05-12 21:30:27 +0800

    Deployment Preparation

---
+++ backend/first-spring/src/main/java/com/spring/Controller/ForwardController.java
@@ -0,0 +1,13 @@
+package com.spring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ForwardController {
+
+    @GetMapping(value = "/{path:[^\\.]*}")
+    public String redirect(){
+        return "forward:/";
+    }
+}
